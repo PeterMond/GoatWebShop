f8bc6b9a4411b811120dbec285130e8c2cfeefd4
Pascal van Egmond <pascalvanegmond@hotmail.com>
2017-06-09 17:03:39 +0200

    Add project files.

---
+++ GoatWebShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoatWebShop.Startup))]
+namespace GoatWebShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
