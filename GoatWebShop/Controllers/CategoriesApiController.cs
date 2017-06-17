522130500df78587ddc5aef54765786b22f3269a
Pascal van Egmond <pascalvanegmond@hotmail.com>
2017-06-17 20:53:54 +0200

    Products are shown on the home page and added some filters.

---
+++ GoatWebShop/Controllers/CategoriesApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GoatWebShop.Models;
+
+namespace GoatWebShop.Controllers
+{
+    public class CategoriesApiController : ApiController
+    {
+        private ShopEntities db = new ShopEntities();
+
+        // GET: api/CategoriesApi
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories;
+        }
+
+    }
+}
--- GoatWebShop/Controllers/ProductsApiController.cs
+++ GoatWebShop/Controllers/ProductsApiController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Web.Http;
 using System.Web.Http.Description;
 using GoatWebShop.Models;
@@ -21,6 +22,54 @@ namespace GoatWebShop.Controllers
         {
             return db.Products;
         }
+
+        // GET: api/ProductsApi/GetProductsFromCategory/1
+        [Route("api/ProductsApi/GetProductsFromCategory/{category}")]
+        [HttpGet]
+        public IQueryable<Product> GetProductsFromCat(int category)
+        {
+            return db.Products.Where(p => p.Category_ID == category);
+        }
+
+        // GET: api/ProductsApi/GetProductsSorted/1
+        [Route("api/ProductsApi/GetProductsSorted/{sortOption}")]
+        [HttpGet]
+        public IQueryable<Product> GetProductsSorted(int sortOption)
+        {
+            IQueryable<Product> products;
+            switch (sortOption)
+            {
+                case 1:
+                    products = db.Products.OrderBy(p => p.Price);
+                    break;
+
+                case 2:
+                    products = db.Products.OrderByDescending(p => p.Price);
+                    break;
+
+                case 3:
+                    products = db.Products.OrderBy(p => p.Name);
+                    break;
+
+                case 4:
+                    products = db.Products.OrderByDescending(p => p.Name);
+                    break;
+
+                case 5:
+                    products = db.Products.OrderByDescending(p => p.OrderRows.Count());
+                    break;
+
+                case 6:
+                    products = db.Products.OrderByDescending(p => p.AmountInStock).Where(p => p.AmountInStock > 0);
+                    break;
+
+                default:
+                    products = db.Products;
+                    break;
+            }
+            return products;
+        }
+
 
         // GET: api/ProductsApi/5
         [ResponseType(typeof(Product))]
